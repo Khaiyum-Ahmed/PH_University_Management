5c673843d60c242feca7123e27bbfccf938a21d1
Khaiyum-Ahmed <sayedshakin666@gmail.com>
2026-02-26 01:00:06 +0600

    test academic faculty routes using postman

--- src/app/modules/academicFaculty/academicFaculty.validation.ts
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -1,11 +1,16 @@
 import z from 'zod';
 
 const academicFacultyValidationSchema = z.object({
-  name: z.string({ error: 'Academic Faculty must be string' }),
+  body: z.object({
+    name: z.string({ error: 'Academic Faculty must be string' }),
+  }),
 });
 const updateAcademicFacultyValidationSchema = z.object({
-  name: z.string({ error: 'Academic Faculty must be string' }),
+  body: z.object({
+    name: z.string({ error: 'Academic Faculty must be string' }),
+  }),
 });
+
 export const AcademicFacultyValidation = {
   academicFacultyValidationSchema,
   updateAcademicFacultyValidationSchema,
